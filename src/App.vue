10e9b1cff75adb3471e7a34983d5801a53d4be91
AxelMth <aujoulator@hotmail.fr>
2024-11-17 22:13:55 +0100

    add port

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-  const hostname = import.meta.env.PROD ? '192.168.1.118' : 'localhost:3000'  
+  const hostname = import.meta.env.PROD ? '192.168.1.118:3000' : 'localhost:3000'  
   const onBuzz = async () => {
     fetch('http://' + hostname + '/buzz', {
       method: 'GET',
