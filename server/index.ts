aa6adcabe6522b6e9954438be89fcc28a538d93d
AxelMth <aujoulator@hotmail.fr>
2024-12-11 15:09:12 +0100

    use mutex to lock sound play when playig

--- server/index.ts
+++ server/index.ts
@@ -8,7 +8,9 @@ const allowedOrigins: string[] = [
     'http://127.0.0.1:5173',
     'http://localhost:5173',
     'http://192.168.1.118:5173',
-    'http://192.168.1.118:5173/'
+    'http://192.168.1.118:5173/',
+    'http://192.168.1.36:5173',
+    'http://192.168.1.36:5173/'
 ]
 
 app.use(cors({
@@ -26,16 +28,33 @@ app.use(cors({
     }
 }))
 
+let isPlaying = false;
+
 app.get('/buzz', (req: express.Request, res: express.Response) => {
+    if (isPlaying) {
+        res.status(429).send('Another sound is currently playing')
+        return
+    }
+    isPlaying = true;
+
     const { userId } = req.query;
-    child_process.exec(`python play_sound.py /home/amathieu/secret-house/public/${userId}.mp3`, (error, stdout) => {
-        if (error) {
-            console.error(`exec error: ${error}`)
-            res.status(500).send('error')
-            return
-        }
-        res.send(stdout)
+    const play = child_process.spawn('python', [`play_sound.py`,`public/${userId}.mp3`]);
+    
+    play.on('close', () => {
+        console.log('Sound played')
+        isPlaying = false;
     })
+
+    play.stdout.on('data', (data) => {  
+        console
+        .log(`stdout: ${data}`);
+    })
+    
+    play.on('error', (err) => {
+        console.error(err)
+        isPlaying = false;
+    })
+    res.send('Sound played')
 })
 
 app.listen(3000, () => {
